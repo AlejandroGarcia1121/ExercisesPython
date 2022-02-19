1597ca59fda6fc50f100f2508152ffe1fb9192c8
AlejandroGarcia1121 <alejandro.garcias1@udea.edu.co>
2022-02-19 13:30:43 -0500

    Ex1

---
+++ py.checkio.py
@@ -0,0 +1,14 @@
+"""
+This mission is the easiest one. Write a function that will receive 2 numbers as input and it should return the multiplication of these 2 numbers.
+
+    Input: Two arguments. Both are of type int.
+    Output: Int.
+
+    Example:
+        mult_two(2, 3) == 6
+        mult_two(1, 0) == 0
+"""
+def mult_two(a, b):
+    # your code here
+    return a * b
+print(mult_two(2, 3))
